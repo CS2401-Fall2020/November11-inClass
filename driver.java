ea8e8beb5e60a8d898152daa1b492a4d657d809b
Dan DeBlasio <dan@dandeblasio.com>
2020-11-11 11:26:42 -0700

    inClass

--- driver.java
+++ driver.java
@@ -11,7 +11,41 @@ public class driver {
       list.deleteTail();
       //list.deleteHead();
     }
-
+    
+    // Stack Tests
+    StackLL myStack = new StackLL();
+    System.out.println("Stack Empty: " + myStack.isEmpty() + "\tLength: " + myStack.getLength() + "\tpeek:  " + myStack.peek() );
+    myStack.push("String 1");
+    System.out.println("Stack Empty: " + myStack.isEmpty() + "\tLength: " + myStack.getLength() + "\tpeek:  \"" + myStack.peek() + "\"" );
+    myStack.push("String 2");
+    System.out.println("Stack Empty: " + myStack.isEmpty() + "\tLength: " + myStack.getLength() + "\tpeek:  \"" + myStack.peek() + "\"" );
+    myStack.push("String 3");
+    System.out.println("Stack Empty: " + myStack.isEmpty() + "\tLength: " + myStack.getLength() + "\tpeek:  \"" + myStack.peek() + "\"" );
+    System.out.println("pop: \"" + myStack.pop() + "\"");
+    System.out.println("Stack Empty: " + myStack.isEmpty() + "\tLength: " + myStack.getLength() + "\tpeek:  \"" + myStack.peek() + "\"" );
+    
+    System.out.println("pop: \"" + myStack.pop() + "\"");
+    System.out.println("pop: \"" + myStack.pop() + "\"");
+    System.out.println("pop: \"" + myStack.pop() + "\"");
+    
+    System.out.println("Stack Empty: " + myStack.isEmpty() + "\tLength: " + myStack.getLength() + "\tpeek:  \"" + myStack.peek() + "\"" );
+    
+    
+    // Queue Tests
+    QueueLL myQueue = new QueueLL();
+    System.out.println("Queue empty? " + myQueue.isEmpty() + "\tLength: " + myQueue.getLength() + "\tpeek: \""+myQueue.peek()+"\"");
+    myQueue.enqueue("String 1");
+    System.out.println("Queue empty? " + myQueue.isEmpty() + "\tLength: " + myQueue.getLength() + "\tpeek: \""+myQueue.peek()+"\"");
+    myQueue.enqueue("String 2");
+    System.out.println("Queue empty? " + myQueue.isEmpty() + "\tLength: " + myQueue.getLength() + "\tpeek: \""+myQueue.peek()+"\"");
+    myQueue.enqueue("String 3");
+    System.out.println("Queue empty? " + myQueue.isEmpty() + "\tLength: " + myQueue.getLength() + "\tpeek: \""+myQueue.peek()+"\"");
+    System.out.println("Dequeue: " + myQueue.dequeue());
+    System.out.println("Dequeue: " + myQueue.dequeue());
+    System.out.println("Dequeue: " + myQueue.dequeue());
+    System.out.println("Dequeue: " + myQueue.dequeue());
+    System.out.println("Queue empty? " + myQueue.isEmpty() + "\tLength: " + myQueue.getLength() + "\tpeek: \""+myQueue.peek()+"\"");
+    
   }
 
 }
